ac82ebeba847a236a98bcad2343fa455658b50cd
Varun Sharma <varun_sharma@live.co.uk>
2024-09-14 14:10:35 -0400

    feat: update packages for site - attempt to fix build issue on vercel

---
+++ src/components/ui/Cards/LogoCard/logoCard.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { BorderBeam } from '@/components/magicui/border-beam';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+
+export default function LogoCard() {
+    const { theme } = useTheme();
+
+    return (
+        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+            {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+                Ve
+            </span> */}
+
+            {theme === 'dark' ? (
+                <Image src="/logos/ve_technologies_dark_mode.png" width={500} height={500} alt="Ve Dark Mode Logo" />
+            ) : (
+                <Image src="/logos/ve_technologies_light_mode.png" width={500} height={500} alt="Ve Light Mode Logo" />
+            )}
+
+            <BorderBeam size={250} duration={12} delay={9} />
+        </div>
+    );
+}
