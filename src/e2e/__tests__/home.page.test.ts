e78f7286193facfe1fcba20e19cbc6956ca69738
Varun Sharma <62218708+ve-varun-sharma@users.noreply.github.com>
2024-12-31 22:09:30 -0800

    feat: add additional tests

---
+++ src/e2e/__tests__/home.page.test.ts
@@ -0,0 +1,3 @@
+import { shortest } from '@antiwork/shortest';
+
+shortest('Click the light mode button on the bottom Nav bar and light mode for the app turns on, and default dark mode is off.');
--- src/e2e/__tests__/navbar.test.ts
+++ src/e2e/__tests__/navbar.test.ts
@@ -1,3 +1,5 @@
 import { shortest } from '@antiwork/shortest';
 
 shortest('Click the Home button on the bottom Nav bar and brings the user to the home page');
+
+shortest('Click the Project button on the bottom Nav bar and brings the user to the #projects section');
